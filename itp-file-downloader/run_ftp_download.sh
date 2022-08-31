c95801aa4485df75b44d41ff8eea7fe11c400f8e
eesh meerkat <eeshan137@gmail.com>
2022-08-31 12:05:13 -0400

    instructions to work with Python 3.9

--- itp-file-downloader/run_ftp_download.sh
+++ itp-file-downloader/run_ftp_download.sh
@@ -1,12 +1,23 @@
 #!/usr/bin/env bash
 
+# python scripts break in python 3.10
+# solution: use anaconda
+# conda create --name snakes python=3.9
+# conda activate snakes
+# ./run_ftp_download
+# conda deactivate
+#
+#
+# must install anaconda before!
+# https://conda.io/projects/conda/en/latest/user-guide/install/index.html
+
 # modify OPENSSL configuration if 20.04
 if [[ $(lsb_release -rs) == "20.04" ]]; then
 	echo "using local OPENSSL_CONF"
 	export OPENSSL_CONF=$(pwd)/openssl.cnf
 fi
 
-if [[ $(lsb_release -rs) == "21.04" ]]; then
+if [[ $(lsb_release -rs) == "22.04" ]]; then
 	echo "using local OPENSSL_CONF"
 	export OPENSSL_CONF=$(pwd)/openssl.cnf
 fi
