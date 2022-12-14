8857983ecdf19a26744a08572da93274420b223e
eesh xps <eeshan137@gmail.com>
2022-12-14 16:11:41 -0500

    compatability with using wget or python download script

--- matlab-indexer/index_itp_data.m
+++ matlab-indexer/index_itp_data.m
@@ -3,35 +3,82 @@
 % access later on
 
 %% set path to ITP directory
-itpFolders = dir('~/Data/itp/');
+itpFolders = dir('~/Code/itp-articuno/itp-data');
 dirFlag = [itpFolders.isdir];
 dirFlag(1:2) = 0; % removes . and .. as entries
 
 % isolate folders only
 itpFolders = itpFolders(dirFlag);
-N = numel(itpFolders);
+NF = numel(itpFolders);
+
+%% loop through N files to make final list between *final and *grddata
+for nf = 1:NF
+    itpNum{nf} = erase(itpFolders(nf).name,["final","grddata"]);
+end
+
+% empty count
+count = 0;
+
+for nf = 1:NF
+    
+    % check to see how many copies there are
+    listBool = strcmpi(itpNum{nf},itpNum);
+    fCount = sum(listBool);
+    
+    % only grddata exists
+    if fCount == 1
+        count = count + 1;
+        itpList{count} = [itpNum{nf} 'grddata'];
+    elseif fCount == 2
+        count = count + 1;
+        itpList{count} = [itpNum{nf} 'final'];
+    end
+end
+
+itpList = unique(itpList);
+
+%% make ITP struct
 
 % overall structure - will be large
 ITP = [];
+N = numel(itpList);
 k = 0;
+
 for n = 1:N
     
-    datFiles = dir([itpFolders(n).folder '/' itpFolders(n).name '/*grd*.dat']);
-    M = numel(datFiles);
+    datFiles = dir([itpFolders(n).folder '/' itpList{n} '/*grd*.dat']);
     
     if isempty(datFiles)
         fprintf('\n');
-        warning('%s is empty',itpFolders(n).name);
+        warning('%s is empty',itpList{n});
+        
+        % try other thing for funsies
+        temp = itpList{n};
+        cc = contains(temp,'f');
+        if cc
+            temp2 = split(temp,'f');
+            check = [temp2{1} 'grddata'];
+        else
+            temp2 = split(temp,'g');
+            check = [temp2{1} 'final'];
+        end
+        
+        datFiles = dir([itpFolders(n).folder '/' check '/*grd*.dat']);
+
+        if isempty(datFiles)
+            warning('%s is also empty!',check);
+        end
     end
     
     tic
+    M = numel(datFiles);
     for m = 1:M
         mCheck = ceil([0.1 0.3 0.5 0.7 0.9]*M);
         k = k+1;
         
         % assign ITP machine number
         eraseStr1 = ["final","grddata","itp"];
-        itpNumStr = erase(itpFolders(n).name,eraseStr1);
+        itpNumStr = erase(itpList{n},eraseStr1);
         ITP(k).machine = itpNumStr;
         if m == 1
            fprintf('\n ITP %4s',itpNumStr);
@@ -39,14 +86,14 @@ for n = 1:N
         
         % assign data "status" = final (tier3 data), grddata (tier2 data or tier1, but either way, not tier3)
         eraseStr2 = ["itp",itpNumStr];
-        itpStatusStr = erase(itpFolders(n).name,eraseStr2);
+        itpStatusStr = erase(itpList{n},eraseStr2);
         if strcmp(itpStatusStr,"final")
             ITP(k).status = 3;
         elseif strcmp(itpStatusStr,"grddata")
             ITP(k).status = 2;
         else
             ITP(k).status = 0;
-            warning('ITP status = 0 @ %s', itpFolders(n).name);
+            warning('ITP status = 0 @ %s', itpList{n});
         end
         
         % info extract from *profile*, not buoy
